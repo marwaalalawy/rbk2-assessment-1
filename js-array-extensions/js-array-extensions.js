6be3a580b9bcddf8310090ccc9e609740accb3c1
marwa <marwa.alalawy@gmail.com>
2017-01-07 10:10:13 +0200

    done

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -1,13 +1,13 @@
 // your code here
-var Array=function () {
+var ArrayExtension=function () {
 	this.store=[];	
 }
-array.prototype.first=function(){
+ArrayExtension.prototype.first=function(){
 	var f=this.store[0];
 	return f;
 }
 
-array.prototype.last=function(){
+ArrayExtension.prototype.last=function(){
 	var l=this.store[this.store.length-1]
 	return l;
 }
