2203348de83b09022505e6441dc0da82c18c85e1
marwa <marwa.alalawy@gmail.com>
2017-01-07 09:21:05 +0200

    done

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,3 +1,17 @@
 var Stack = function() {
-	//your code is here
+	var instance=Object.create(methods);
+	instance._storage=[];
+	return instance;
 };
+
+var methods={};
+
+methods.add=function(value){
+	this._storage.push(value);
+}
+
+methods.remove=function(){
+	if(this.counter>0){
+		this._storage.pop();
+	}
+}
